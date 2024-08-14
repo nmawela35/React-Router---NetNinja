b79d9de8f6d711c51245f3ac5cfb2e7cc39de3b6
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-14 20:00:20 +0200

    Tutorial 1-2 complete

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    
+    <div>
+        <h2>About Us</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dicta voluptatem dolor voluptates totam velit possimus in ipsam magnam ducimus.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dicta voluptatem dolor voluptates totam velit possimus in ipsam magnam ducimus.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dicta voluptatem dolor voluptates totam velit possimus in ipsam magnam ducimus.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home' >
+        <h2>Welcome</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dicta voluptatem dolor voluptates totam velit possimus in ipsam magnam ducimus.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dicta voluptatem dolor voluptates totam velit possimus in ipsam magnam ducimus.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dicta voluptatem dolor voluptates totam velit possimus in ipsam magnam ducimus.</p>
+        
+    </div>
+  )
+}
+
+export default Home
