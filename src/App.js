fc4f7d4afc92e78702e60852ae908591817aa685
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-15 08:50:06 +0200

    createBrowserRouter intro

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {createBrowserRouter, BrowserRouter, Route, NavLink, createRoutesFromElements, RouterProvider } from "react-router-dom";
 
 // pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from "./pages/Contact";
 
+
+
 function App() {
-  return (
-    <BrowserRouter>
+  const newRouter = createBrowserRouter(createRoutesFromElements(
+      <Route>
+        <Route path="home" element={<Home/>}/>
+        <Route path ="about" element={<About/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+      </Route>
+  ))
+  
+    return (
+      <>
+      <BrowserRouter>
+
     <header>
       <nav>
         <h1>JobaRouter</h1>
-        <NavLink to="/">Home</NavLink>
+        <NavLink path="/" to="home">Home</NavLink>
         <NavLink to= "about">About</NavLink>
         <NavLink to= "contact">Contact</NavLink>
       </nav>
     </header>
+      </BrowserRouter>
 
     <main>
-      <Routes>
-        <Route path="home" element={<Home/>}/>
-        <Route path ="about" element={<About/>}/>
-        <Route path="/Contact" element={<Contact/>}/>
-      </Routes>
+      <RouterProvider router={newRouter}/>
     </main>
+      </>
 
-    </BrowserRouter>
   );
 }
 
