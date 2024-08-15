6ff1df062146edf53dd9faa389a22d522610cf3c
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-15 07:00:28 +0200

    Contact page added, moving to createBrowser router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 // pages
 import Home from './pages/Home'
 import About from './pages/About'
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -12,13 +13,15 @@ function App() {
         <h1>JobaRouter</h1>
         <NavLink to="/">Home</NavLink>
         <NavLink to= "about">About</NavLink>
+        <NavLink to= "contact">Contact</NavLink>
       </nav>
     </header>
 
     <main>
       <Routes>
-        <Route index element={<Home/>}/>
+        <Route path="home" element={<Home/>}/>
         <Route path ="about" element={<About/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
       </Routes>
     </main>
 
---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+        <h2>Contact form</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dicta voluptatem dolor voluptates totam velit possimus in ipsam magnam ducimus.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dicta voluptatem dolor voluptates totam velit possimus in ipsam magnam ducimus.</p>
+    </div>
+  )
+}
+
+export default Contact
